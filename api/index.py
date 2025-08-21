2fc6969c29796a6758b14ad46a461575513c1365
T_cat9000_2 <terpstragraham@gmail.com>
2025-08-21 15:34:23 -0700

    just the good stuff ok.

--- api/index.py
+++ api/index.py
@@ -3,10 +3,6 @@ import scratchattach as sa
 
 app = Flask(__name__, template_folder="../templates")
 
-@app.route("/static/favicon.ico")
-def favicon():
-    return app.send_static_file("favicon.ico")
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     output = None
@@ -19,5 +15,6 @@ def index():
             user_input = f"User does not exist or another error."
         output = f"{user_input}"
     return render_template("index.html", output=output)
+
 
 
